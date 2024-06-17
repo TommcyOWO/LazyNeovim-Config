1f5016609089776477935ef69a642fac8b1e799d
TommcyOWO <tangminghong.tom@gmail.com>
2024-06-17 22:29:50 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+
+require('keymaps')      
+require('options')
+require('plugins')
+
+--configs of plugins
+-- catppuccin
+vim.cmd.colorscheme "catppuccin"
+
+-- telescope 
+local builtin = require('telescope.builtin')
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
